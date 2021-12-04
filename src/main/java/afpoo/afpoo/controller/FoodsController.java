94cebec7f5863c9e31a0653b8ed7f5b6f8193e6f
Guilherme Saracura <guilhermesaracura@gmail.com>
2021-12-03 21:32:13 -0300

    Add CrossOrigin

--- src/main/java/afpoo/afpoo/controller/FoodsController.java
+++ src/main/java/afpoo/afpoo/controller/FoodsController.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -21,6 +22,7 @@ import afpoo.afpoo.dto.FoodDTO;
 import afpoo.afpoo.model.Food;
 import afpoo.afpoo.service.FoodsService;
 
+@CrossOrigin
 @RestController
 @RequestMapping("Foods")
 public class FoodsController {
